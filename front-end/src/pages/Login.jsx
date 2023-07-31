0ee93874e60e73207d4a97da32d3b5360218e3c7
Erwin Darsono <erwindarsono08@gmail.com>
2023-08-01 01:32:43 +0700

    add font and styling

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,31 @@
-import Header from '../components/login/Header'
-import Footer from '../components/login/Footer'
+import Header from "../components/login/Header";
+import Footer from "../components/login/Footer";
 
 function Login() {
   return (
     <>
       <Header />
+      <main>
+        <div className="square">
+          <h1>Sign in</h1>
+          <form action="/login" method="post">
+            <div className="credentials">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" />
+            </div>
+            <div className="credentials">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+            </div>
+            <div id="btn_padding">
+              <input type="submit" value="Sign in" id="submit" />
+            </div>
+            <a href="/register" id="register">
+              Register
+            </a>
+          </form>
+        </div>
+      </main>
       <Footer />
     </>
   );
